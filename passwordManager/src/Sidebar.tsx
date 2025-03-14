f83231b4cf710382e484c49cdc0d0cca5df7e3dd
Ruchir <Ruchir@webdev08.local>
2025-03-14 13:43:08 -0400

    working on tab

--- passwordManager/src/Sidebar.tsx
+++ passwordManager/src/Sidebar.tsx
@@ -2,7 +2,7 @@ import Tab from "./Tab";
 
 function Sidebar() {
     return (
-        <div className="col-span-1 p-4 bg-gray-300">
+        <div className="col-span-1 p-4 bg-gray-400">
             <div className="w-full h-full grid gap-4 auto-rows-[10%] p-4 default-border">
                 <Tab></Tab>
             </div>
--- passwordManager/src/Tab.tsx
+++ passwordManager/src/Tab.tsx
@@ -1,11 +1,7 @@
-function log() {
-    console.log("super");
-}
-
 function Tab() {
     return (
         <div className="flex w-full justify-center">
-            <button className="w-[95%] h-full default-border" onClick={log}></button>
+            <button className="w-[95%] h-full default-border"></button>
         </div>
     )
 }
