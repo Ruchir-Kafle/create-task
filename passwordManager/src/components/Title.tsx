d98d95a67f3bf016d3fc57955717c00def043db0
Ruchir Kafle <ruchirkafle@gmail.com>
2025-03-17 11:54:00 -0400

    task add button

--- passwordManager/src/components/Title.tsx
+++ passwordManager/src/components/Title.tsx
@@ -4,8 +4,12 @@ interface Props {
 
 function Title({children}: Props) {
     return (
-        <div className="row-span-1 row-start-1 row-end-1 flex items-center justify-center">
-            <h1 className="text-3xl w-[75%] text-center border-b-2 p-2">{children}</h1>
+        <div className="row-span-1 row-start-1 row-end-1 custom-grid">
+            <h1 className="col-start-2 text-3xl text-center border-b-2 p-1">{children}</h1>
+            
+            <div className="col-start-3 flex items-center justify-center">
+                <button className="w-[42.5px] h-[42.5px] flex items-center justify-center bg-green-400 default-border rounded-xl">+</button>
+            </div>
         </div>
     )
 }
