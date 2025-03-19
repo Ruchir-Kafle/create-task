02a9849354b0b12a41aeabb0755e3c8c5be01283
Ruchir Kafle <ruchirkafle@gmail.com>
2025-03-19 12:50:31 -0400

    finished json, working on it properly being rendered

--- passwordManager/src/components/PasswordBody.tsx
+++ passwordManager/src/components/PasswordBody.tsx
@@ -5,13 +5,24 @@ import Title from "./Title";
 interface Props {
     setClosed: React.Dispatch<React.SetStateAction<boolean>>;
     setType: React.Dispatch<React.SetStateAction<string>>;
+    selectedTab: string;
+    accounts: Array<object>;
 }
 
-function PasswordBody({setClosed, setType}: Props) {
+function PasswordBody({setClosed, setType, selectedTab, accounts}: Props) {
+
+    const createTabs = () => {
+        let websites = [];
+        for (let account of accounts) {
+            websites.push(<Tab selectedTab={selectedTab} tabData={account}></Tab>);
+        }
+        return websites;
+    }
+
     return (
         <Section className="col-span-2 m-4">
             <Title setClosed={() => setClosed(false)} setType={setType}>Accounts & Passwords</Title>
-            <Tab setClosed={setClosed} setType={setType}></Tab>
+            {...createTabs()}
         </Section>
     )
 }
--- passwordManager/src/components/Sidebar.tsx
+++ passwordManager/src/components/Sidebar.tsx
@@ -1,18 +1,30 @@
 import Tab from "./Tab";
 import Title from "./Title";
 import Section from "./Section";
+import { ReactNode } from "react";
 
 interface Props {
     setClosed: React.Dispatch<React.SetStateAction<boolean>>;
     setType: React.Dispatch<React.SetStateAction<string>>;
+    setSelectedTab: React.Dispatch<React.SetStateAction<string>>;
+    websites: Array<string>;
 }
 
-function Sidebar({setClosed, setType}: Props) {
+function Sidebar({setClosed, setType, setSelectedTab, websites}: Props) {
+
+    const createTabs = () => {
+        let temp: Array<ReactNode> = [];
+        for (let website of websites) {
+            temp.push(<Tab setSelectedTab={setSelectedTab} tabData={website}></Tab>);
+        }
+        return temp;
+    }
+
     return (
         <div className="col-span-1 p-4 bg-gray-400">
             <Section className="w-full h-full">
                 <Title setClosed={setClosed} setType={setType}>Websites</Title>
-                <Tab setClosed={setClosed}></Tab>
+                {...createTabs()}
             </Section>
         </div>
     )
